0056dc368211c5e0a2630f854a117d882774e8d7
Seav Seyla <Mrrseyla.758@gmail.com>
2025-02-09 15:54:02 +0700

    inti

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
-<script>
-export default {
-  data(){
-    return {
-      name:"seav seyla",
-      tasks:["task1","task2","task3"],
-      status:"pending",
-      googleLink:'https://google.com'
-    }
-  }
-}
-</script>
 <template>
-  <header>
-    <p>Hello <span>{{ name }}</span></p>
-    <h1>
-      This Task 
-    </h1>
-    <div>
-      <p v-for="task in tasks" :key="task">{{ task }}</p>
-    </div>
-    <p v-if="status==='pending'">User is pending</p>
-    <p v-else-if="status==='active'">User is active</p>
-    <p v-else>User is Inactive</p>
-
-    <a :href="googleLink">google</a>
-
-  </header>
+  <!-- Navbar -->
+  <!-- Search -->
+  <!-- Notes -->
+  Hello
 </template>
-<style scoped>
-p {
-  color: red;
-}
-span {
-  color: blue;
-}
-</style>
-
