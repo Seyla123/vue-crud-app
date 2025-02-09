e66a571d884f37e7a82ca2f7c7a8e074c112e50c
Seav Seyla <Mrrseyla.758@gmail.com>
2025-02-09 17:27:41 +0700

    feat(card-note) : add note card

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,12 +2,10 @@
 import { Search, Mic } from "lucide-vue-next";
 </script>
 <template>
-  <section class="flex items-center text-gray-600 justify-between text-center px-6 py-4 bg-[#6c63ff]/10 rounded-full">
+  <section class="flex items-center text-gray-600 justify-between text-center px-6 py-4 bg-[#6c63ff]/5 rounded-full">
     <!-- search icon -->
-    <div class="flex gap-1 items-center ">
       <Search size="24" />
-      <span class="text-lg">Search</span>
-    </div>
+      <input class="w-full px-2 text-lg placeholder:opacity-50 focus:outline-none focus:ring-0" placeholder="Search"/>
     <!-- voice icon -->
     <Mic size="24" />
   </section>
