17db8df21213470065886a70ec6ba2bb00d67eb0
Seav Seyla <Mrrseyla.758@gmail.com>
2025-02-09 18:03:37 +0700

    chore(create-note) : start

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,11 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import HomeView from "@/pages/HomeView.vue";
 import NoteView from "@/pages/NoteView.vue";
+import NoteCreate from "@/pages/NoteCreate.vue";
 
 const routes = [
   { path: "/", component: HomeView },
+  {path:'/notes', component: NoteCreate},
   { path: "/notes/:id", component: NoteView },
 ];
 
