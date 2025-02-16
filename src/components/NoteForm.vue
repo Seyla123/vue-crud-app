b54b946b5b1f260009ad51093df974ca98fc22d1
Seav Seyla <Mrrseyla.758@gmail.com>
2025-02-16 10:45:01 +0700

    feat(note-form) : add note form component

---
+++ src/components/NoteForm.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { defineProps, defineEmits, computed } from "vue";
+
+defineEmits(["submit", "update:modelValue"]);
+const props = defineProps({
+  modelValue: Object,
+});
+const note = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
+
+</script>
+<template>
+  <form @submit.prevent="$emit('submit')" action="" class="space-y-4">
+    <div class="flex flex-col">
+      <label for="title" class="text-sm font-semibold">Title</label>
+      <input
+        type="text"
+        placeholder="Title"
+        v-model="note.title"
+        class="border-1 p-2 rounded-lg focus:outline-none"
+      />
+    </div>
+    <div class="flex flex-col">
+      <label for="content" class="text-sm font-semibold">Content</label>
+      <textarea
+        placeholder="Content"
+        class="border-1 p-2 rounded-lg focus:outline-none"
+        rows="5"
+        v-model="note.content"
+      ></textarea>
+    </div>
+    <div class="flex flex-col">
+      <label for="tag" class="text-sm font-semibold">Tag</label>
+      <input
+        type="text"
+        placeholder="Tag"
+        class="border-1 p-2 rounded-lg focus:outline-none"
+        v-model="note.tag"
+      />
+    </div>
+    <div class="flex flex-col">
+      <label for="date" class="text-sm font-semibold">Date</label>
+      <input
+        type="date"
+        class="border-1 p-2 rounded-lg focus:outline-none"
+        v-model="note.date"
+      />
+    </div>
+    <button
+      type="submit"
+      class="bg-[#6c63ff] text-white px-4 py-2 rounded-lg hover:bg-[#6c63ff]/90 transition duration-300 ease-in-out"
+    >
+      Update
+    </button>
+  </form>
+</template>
